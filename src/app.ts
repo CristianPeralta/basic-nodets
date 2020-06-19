86388b58ab30441ee5eeeda80b9fb1b742638740
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-19 12:29:26 -0500

    using express Application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-const app = express();
+const app: express.Application = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const greeting: string = 'Hello World';
+    res.send(greeting);
 });
 
 app.listen(3000, () => console.log('Server running, port: 3000'));
