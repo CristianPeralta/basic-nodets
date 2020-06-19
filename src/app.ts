f3ff670d51230d9a5b1737f6452acd8bbe12de7a
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-19 12:38:25 -0500

    update build script

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app: Application = express();
 const getGreeting = (name: string): string => `Hello ${name}`;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send(getGreeting('World'));
+    res.send(getGreeting('World!!'));
 });
 
 app.listen(3000, () => console.log('Server running, port: 3000'));
