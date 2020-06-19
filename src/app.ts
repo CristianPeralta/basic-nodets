af76703c051c8f1740ccdf7e6b36f873b97aabbd
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-19 12:27:20 -0500

    add basic express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-const greeting: string = 'Hello World';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Server running, port: 3000'));
