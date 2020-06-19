f034add54f41c8b5576ffa86f50c60a35af90c71
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-19 12:36:11 -0500

    add getGreeting function

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
+const getGreeting = (name: string): string => `Hello ${name}`;
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const greeting: string = 'Hello World';
-    res.send(greeting);
+    res.send(getGreeting('World'));
 });
 
 app.listen(3000, () => console.log('Server running, port: 3000'));
